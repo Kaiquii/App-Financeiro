b598029f9e8b0af520e08f1b8c2cc47642611b62
Kaiqui <kaiqu@example.com>
2026-03-19 09:42:45 -0300

    adicionado cores padrões

--- app/src/main/java/com/example/appfinanceiro/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/example/appfinanceiro/core/designsystem/theme/Color.kt
@@ -1,2 +1,12 @@
 package com.example.appfinanceiro.core.designsystem.theme
 
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF1877F2)
+val BackgroundDark = Color(0xFF121212)
+val BackgroundLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
+val SurfaceLight = Color(0xFFF0F2F5)
+val TextPrimaryDark = Color.White
+val TextPrimaryLight = Color.Black
+val TextSecondary = Color(0xFFA0AEC0)
